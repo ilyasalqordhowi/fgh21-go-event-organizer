1122098c416e76805ab5ba96f3c3575eb8d62841
ilyas <ilyasalqordhowi@gmail.com>
2024-09-17 18:57:14 +0700

    fix categories

--- routers/categories.go
+++ routers/categories.go
@@ -3,11 +3,9 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/ilyasalqordhowi/fgh21-go-event-organizer/controllers"
-	"github.com/ilyasalqordhowi/fgh21-go-event-organizer/middlewares"
 )
 
 func CategoriesRouter(r *gin.RouterGroup){
-	r.Use(middlewares.AuthMiddleware())
 	r.POST("",controllers.CreateCategory)
     r.GET("", controllers.ListAllCategory)
 	r.GET("/:id",controllers.DetailCategory)
